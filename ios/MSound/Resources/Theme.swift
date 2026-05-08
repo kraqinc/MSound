fb2512efbc225f8cc67efaf1ada779b5d53a100e
kraqinc <kraqai.c@gmail.com>
2026-05-08 17:52:03 -0500

    Add design system theme

---
+++ ios/MSound/Resources/Theme.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+struct MSoundTheme {
+    static let accentColor = Color(red: 0.1, green: 0.8, blue: 0.5)
+    static let backgroundColor = Color(red: 0.05, green: 0.05, blue: 0.05)
+    static let secondaryBackground = Color(red: 0.12, green: 0.12, blue: 0.12)
+    static let textColor = Color.white
+    static let secondaryTextColor = Color.gray
+}
